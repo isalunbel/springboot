5a7e28538ef98ed4cdb6ab4588cd3014559f473f
Isa <lilu6890@gmail.com>
2024-03-12 11:41:48 +0300

    Initial commit

---
+++ src/AppConfig.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false", matchIfMissing = true)
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
---
+++ src/SystemProfile.java
@@ -0,0 +1,4 @@
+public interface SystemProfile {
+    String getProfile();
+}
+
